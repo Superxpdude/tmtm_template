e47f54ba357a6a57dfb3d6d7a70c89a41748658f
Superxpdude <superxpdude@gmail.com>
2017-03-17 13:27:38 -0500

    Added CfgTaskDescriptions and CfgTaskEnhancements. Changed formatting of description.ext Updated description.ext to make use of preprocessor commands where applicable.

--- config/CfgCommunicationMenu.hpp
+++ config/CfgCommunicationMenu.hpp
@@ -1,4 +1,3 @@
 // Communication Menu
 // Used for being able to call in supports or trigger other mission events. Similar to setting triggers to radio alpha/bravo/etc.
 // https://community.bistudio.com/wiki/Arma_3_Communication_Menu
-
