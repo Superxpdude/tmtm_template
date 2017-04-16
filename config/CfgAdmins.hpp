f41e405dfcf835287483fb044656c6c875d0161c
Superxpdude <superxpdude@gmail.com>
2017-04-16 12:12:09 -0500

    Added an example cfgAdmins definition.

---
+++ config/CfgAdmins.hpp
@@ -0,0 +1,10 @@
+// Admin Console Access
+// Admins defined within this file are given access to the "Mirage Coordinator" function within a mission.
+// The ID used is the steam64ID of any players that are to have access to the console, in array format.
+
+class cfgAdmins
+{
+	ids[] = {
+		"76561198031434864"
+	};
+};
