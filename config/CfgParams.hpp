b4f1cbedd314479a84e9c2d8cc693d6eed00316d
Superxpdude <superxpdude@gmail.com>
2017-04-16 12:55:37 -0500

    Added examples for ACE_Settings. Uncomment the definitions to use them. Updated default mission parameters to include prevent instant death. Updated the display of the default mission parameters.

--- config/CfgParams.hpp
+++ config/CfgParams.hpp
@@ -3,37 +3,69 @@
 // https://community.bistudio.com/wiki/Arma_3_Mission_Parameters
 class params
 {
+	class header_ace_medical
+	{
+		title = "===== ACE3 - Medical Settings =====";
+		values[] = {0};
+		texts[] = {""};
+		default = 0;
+	};
 	// Don't change anything in this class
 	class ace_medical_level
 	{
-		title = "ACE3 Medical Level";
+		title = "ACE3 - Medical Level";
 		ACE_setting = 1;
 		values[] = {1,2};
 		texts[] = {"Basic", "Advanced"};
 		default = 1;
 	};
 	// Don't change anything other than the default value
-	class ace_medical_enableRevive
+	class ace_medical_preventInstaDeath
 	{
-		title = "Enable ACE3 Revive";
+		title = "ACE3 - Prevent Instant Death";
 		ACE_setting = 1;
 		values[] = {0,1};
-		texts[] = {"No", "Yes"};
+		texts[] = {"Disabled", "Enabled"};
 		default = 1;
+	};
+	// Don't change anything other than the default value
+	class ace_medical_enableRevive
+	{
+		title = "ACE3 - Enable Revive";
+		ACE_setting = 1;
+		values[] = {0,1};
+		texts[] = {"Disabled", "Enabled"};
+		default = 1;
+	};
+	// Don't change anything other than the default value
+	class ace_medical_maxReviveTime
+	{
+		title = "ACE3 - Revive Bleedout Timer";
+		ACE_setting = 1;
+		values[] = {30,60,90,120,180,240,300};
+		texts[] = {"30 seconds", "60 seconds", "90 seconds", "2 minutes", "3 minutes", "4 minutes", "5 minutes"};
+		default = 120;
 	};
 	// Don't change anything other than the default value
 	class ace_medical_enableUnconsciousnessAI
 	{
-		title = "AI Unconsciousness Setting";
+		title = "ACE3 - AI Unconsciousness";
 		ACE_setting = 1;
 		values[] = {0,1,2};
-		texts[] = {"Instant Death", "50/50", "Always go unconscious first"};
+		texts[] = {"Disabled", "50/50", "Enabled"};
+		default = 0;
+	};
+	class header_mission_settings
+	{
+		title = "===== Mission Settings =====";
+		values[] = {0};
+		texts[] = {""};
 		default = 0;
 	};
 	// Don't change anything other than the default value
 	class map_markers
 	{
-		title = "Enable group tracking on map";
+		title = "SXP - Enable group tracking on map";
 		values[] = {0,1};
 		texts[] = {"Disabled", "Enabled"};
 		default = 1;
@@ -43,7 +75,7 @@ class params
 	// Don't change anything in this class
 	class headlessclient
 	{
-		title = "Enable Headless Client support";
+		title = "SXP - Enable Headless Client support";
 		values[] = {0,1};
 		texts[] = {"Disabled", "Automatic"};
 		default = 1;
