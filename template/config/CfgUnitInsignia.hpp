6cd3ac2367f947e17b9ceae34c63b7092799be58
Superxpdude <superxpdude@gmail.com>
2017-09-05 13:21:03 -0500

    Added CfgUnitInsignia.hpp to the template folder. Added the insigia images to the template folder.

---
+++ template/config/CfgUnitInsignia.hpp
@@ -0,0 +1,10 @@
+// Unit Insignias
+// Usually visible on the sleeve of a unit
+// https://community.bistudio.com/wiki/Arma_3_Unit_Insignia
+
+class tmtm
+{
+	displayName = "TMTM";
+	author = "TMTM";
+	texture = "template\media\tmtm.paa";
+};
