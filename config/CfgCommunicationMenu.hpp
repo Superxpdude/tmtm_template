b265eeaa7ea8825f618fa4013a6f1bed2924756e
Superxpdude <superxpdude@gmail.com>
2017-09-12 15:35:25 -0500

    Moved all config classes back into description.ext.

--- config/CfgCommunicationMenu.hpp
+++ config/CfgCommunicationMenu.hpp
@@ -1,3 +1,16 @@
 // Communication Menu
 // Used for being able to call in supports or trigger other mission events. Similar to setting triggers to radio alpha/bravo/etc.
 // https://community.bistudio.com/wiki/Arma_3_Communication_Menu
+
+/*
+class exampleCommMenuItem
+{
+	text = "Example Item";
+	submenu = "";
+	expression = "player setDamage 1";
+	icon = "";
+	cursor = "";
+	enable = "true"; // Expression for enabling item
+	removeAfterExpressionCall = 1;
+};
+*/
--- config/CfgTaskEnhancements.hpp
+++ config/CfgTaskEnhancements.hpp
@@ -1,10 +1,8 @@
 // Task Enhancements
 // Enhancements to the task system introduced with the End Game scenarios. Includes task sharing and new markers
-class CfgTaskEnhancements
-{
-	enable = 1; 			// Enable new task features
-	3d = 1; 				// Use 3d task markers
-	3dDrawDist = 2000; 		// Range at which to draw markers. Default is 2000
-	share = 1; 				// Count players assigned to a task and share over the network
-	propagate = 0; 			// Propagate tasks to subordinates when a group leader changes their active task 
-};
+
+enable = 1; 			// Enable new task features
+3d = 1; 				// Use 3d task markers
+3dDrawDist = 2000; 		// Range at which to draw markers. Default is 2000
+share = 1; 				// Count players assigned to a task and share over the network
+propagate = 0; 			// Propagate tasks to subordinates when a group leader changes their active task 
