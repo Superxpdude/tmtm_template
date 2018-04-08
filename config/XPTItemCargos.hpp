d997a32b0aff48e7a88bd5d3f8923f875cbd98ba
Superxpdude <superxpdude@gmail.com>
2018-04-08 17:10:15 -0500

    Added config files for vehicle loadouts, and item cargos.

---
+++ config/XPTItemCargos.hpp
@@ -0,0 +1,12 @@
+// XPTItemCargos.hpp
+// Used for defining ammo box and vehicle item cargos
+// XPTVehicleLoadouts can pull definitions from here in order to supply vehicles
+// Supports sub-class randomization, if a definition has multiple sub-classes, the script will automatically select one of them to apply to the vehicle/box.
+
+class itemCargos
+{
+	class example
+	{
+		
+	};
+};
---
+++ config/XPTVehicleLoadouts.hpp
@@ -0,0 +1,11 @@
+// XPTVehicleLoadouts.hpp
+// Used for defining advanced vehicle loadouts
+// The template can be configured to automatically assign vehicle loadouts on mission start, vehicle respawn, and zeus spawn.
+
+class vehicleLoadouts
+{
+	class example
+	{
+		
+	};
+};
