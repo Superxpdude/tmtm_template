3425d31cfe8d95f2e1d60ea4a525b4b5c24fc67a
Superxpdude <superxpdude@gmail.com>
2020-10-19 13:44:54 -0500

    Added a HEMTT build file and relevant git files.

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+name = "TMTM Template";
+//picture = "";
+actionName = "Github";
+action = "https://github.com/Superxpdude/tmtm_template";
+description = "TMTM Template";
+//logoSmall = "";
+//logo = "";
+//logoOver = "";
+tooltip = "TMTM Template";
+tooltipOwned = "TMTM Template";
+overview = "A mission making framework for TMTM operations.";
+author = "Superxpdude";
+hideName = 0;
+hidePicture = 0;
