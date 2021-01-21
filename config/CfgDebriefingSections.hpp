d92d14dc85c936805239acc77448841e458fa091
Superxpdude <superxpdude@gmail.com>
2021-01-21 08:47:51 -0600

    Added config files for CfgDebriefingSections. Added the ACEX Kill Tracker to CfgDebriefingSections by default.

---
+++ config/CfgDebriefingSections.hpp
@@ -0,0 +1,8 @@
+// CfgDebriefingSections.hpp
+// Defines custom "debriefing sections". These can be used to display additional information at the end of a mission.
+// Has the ACEX Kill Tracker enabled by default.
+
+class acex_killTracker {
+	title = "ACEX Killed Events";
+	variable = "acex_killTracker_outputText";
+};
