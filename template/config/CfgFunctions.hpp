8361c6907cff5fef8fdde6d5302f8f7c14ca3c9e
erem2k <nope@nohost.com>
2023-07-04 19:42:42 -0400

    Abstract loadout resolution code into getLoadoutArray

--- template/config/CfgFunctions.hpp
+++ template/config/CfgFunctions.hpp
@@ -42,6 +42,7 @@ class XPT
 	class loadout
 	{
 		file = "template\functions\loadout";
+		class getLoadoutArray {};				// Resolves loadout array for loadInventory
 		class exportInventory {};				// Exports a unit's inventory to the XPTLoadouts config format
 		class loadCurrentInventory {};			// Uses loadInventory to apply a loadout to a unit
 		class loadInventory {};					// Function for loading a unit's inventory
